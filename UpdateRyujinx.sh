e4053a9d36d0ad6bc481d3b381536c892f1da50f
RNA <47921907+rna0@users.noreply.github.com>
2023-05-12 15:50:24 +0300

    Update UpdateRyujinx.sh
    fixes a change ryujinx made to the downloading api, should work again.

--- UpdateRyujinx.sh
+++ UpdateRyujinx.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd ~/Applications
 rm -rf ./publish
-curl -s https://api.github.com/repos/Ryujinx/release-channel-master/releases/latest | jq -r ".assets[4] | .browser_download_url" | wget -qO ./ryu.tar.gz  -i -
+curl -s https://api.github.com/repos/Ryujinx/release-channel-master/releases/latest | jq -r ".assets[5] | .browser_download_url" | wget -qO ./ryu.tar.gz  -i -
 tar -xzf ./ryu.tar.gz
 rm ./ryu.tar.gz
 sed -i 's|emuName="Ryujinx" #parameterize me|emuName="Ryujinx.Ava" #parameterize me|' /run/media/mmcblk0p1/Emulation/tools/launchers/ryujinx.sh
